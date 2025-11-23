f9db05fe191720215d2452ffe998a4fb699614fd
BeiruthDEV <135391346+BeiruthDEV@users.noreply.github.com>
2025-11-23 14:23:27 -0300

    config

---
+++ src/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    APP_NAME: str = "TransFlow API"
+    APP_VERSION: str = "1.0.0"
+    
+    MONGO_URL: str = "mongodb://mongo:27017"
+    MONGO_DB_NAME: str = "transflow_db"
+    
+    REDIS_HOST: str = "redis"
+    REDIS_PORT: int = 6379
+    
+    RABBITMQ_URL: str = "amqp://guest:guest@rabbitmq:5672/"
+    
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
